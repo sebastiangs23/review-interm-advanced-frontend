3df53a42ad9e31060128c651faf3cf21fdb045c4
sebastiangs23 <sebastiangs2309@gmail.com>
2026-02-03 17:22:14 +0400

    style: :lipstick: style: Improvements in the aside

--- app/layouts/modules.vue
+++ app/layouts/modules.vue
@@ -7,6 +7,9 @@ import { logOut } from "../utils/utils";
 import { useSettingsStore } from "../../stores/settings";
 import ShowCode from "../components/ShowCode.vue";
 import { subModules } from "../utils/common";
+import logo from "../assets/sg-logo.png";
+
+import { Bars3Icon, ChevronLeftIcon } from "@heroicons/vue/24/outline";
 
 const settings = useSettingsStore();
 const router = useRouter();
@@ -17,6 +20,7 @@ definePageMeta({
   layout: "modules",
 });
 
+const isSidebarOpen = ref(false);
 const isActive = ref(false);
 
 const logOutFn = () => {
@@ -25,24 +29,39 @@ const logOutFn = () => {
 };
 </script>
 <template>
-  <div class="flex min-h-screen font-bold">
-    <aside class="flex flex-col pr-6 w-60 bg-[var(--bg-color-secondary)]">
-      <h2 class="font-bold text-(--color-base) text-2xl mt-6 mb-6 text-center">
-        Admin Panel
-      </h2>
+  <div class="flex min-h-screen font-bold flex-col md:flex-row">
+    <aside
+      class="fixed md:static inset-y-0 left-0 z-40 w-60 bg-[var(--bg-color-secondary)] flex flex-col transform transition-transform duration-300"
+      :class="[
+        isSidebarOpen ? 'translate-x-0' : '-translate-x-full',
+        'md:translate-x-0',
+      ]"
+    >
+      <div class="flex items-center justify-between px-4 py-3 text-[var(--color-base)] md:hidden">
+        <img :src="logo" alt="logo" class="w-[4rem]">
+        <h2 class="text-lg w-[8rem] font-bold">Admin Panel</h2>
 
-      <nav class="font-(--font-base) flex flex-col gap-10 mt-6">
+        <button @click="isSidebarOpen = false">
+          <ChevronLeftIcon class="w-6 h-6" />
+        </button>
+      </div>
+
+      <header class="hidden md:flex items-center  py-3 text-[var(--color-base)]">
+        <img :src="logo" alt="logo" class="w-[5rem]">
+        <h2 class="text-xl font-bold">Admin Panel</h2>
+      </header>
+
+      <nav class="font-[var(--font-base)] flex flex-col gap-4 mt-6 pr-6">
         <NuxtLink
           v-for="sub in subModules"
-          :to="sub.to"
-          active-class="btn__base_2 btn__shadow"
-          class="text-(--color-text-primary) text-xl pl-4 inline-flex"
           :key="sub.route"
+          :to="sub.to"
+          active-class="btn__base_2 btn__shadow border-2 border-[var(--color-dark)]"
+          class="flex items-center gap-3 text-[var(--color-text-primary)] text-lg pl-4 py-4 "
+          @click="isSidebarOpen = false"
         >
-          <component :is="sub.icon" class="w-5 h-5 shrink-0 mr-2" />
-          <span>
-            {{ sub.name }}
-          </span>
+          <component :is="sub.icon" class="w-5 h-5 shrink-0" />
+          <span>{{ sub.name }}</span>
         </NuxtLink>
       </nav>
     </aside>
@@ -54,28 +73,19 @@ const logOutFn = () => {
       }"
     >
       <header
-        class="flex items-center justify-between gap-4 px-4 py-3 bg-white rounded-[14px]"
+        class="flex items-center justify-between gap-4 px-3 md:px-4 py-3 bg-white rounded-[14px]"
       >
+        <!-- Mobile toggle -->
+        <button class="md:hidden p-2" @click="isSidebarOpen = !isSidebarOpen">
+          <Bars3Icon class="w-6 h-6" />
+        </button>
+
         <ShowCode />
 
-        <button
-          @click="logOutFn()"
-          :style="{
-            background: `linear-gradient(90deg, ${colorButtons}, #4fadbe)`,
-            border: `none`,
-            color: `white`,
-            [border - radius]: `0.4rem`,
-            padding: `0.5rem 1rem`,
-            [font - weight]: 600,
-            cursor: `pointer`,
-          }"
-          class="modules__logout"
-        >
-          Logout
-        </button>
+        <button @click="logOutFn()" class="modules__logout">Logout</button>
       </header>
 
-      <main class="flex-1 p-[2rem]">
+      <main class="flex-1 p-4 md:p-[2rem]">
         <slot />
       </main>
     </section>
