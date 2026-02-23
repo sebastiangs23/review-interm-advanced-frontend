f7e8b878da38bee03ea4f1cff5589c37be383904
sebastiangs23 <sebastiangs2309@gmail.com>
2026-02-23 22:37:50 +0400

    feat: :sparkles: feat: index.vue finished

---
+++ app/components/Divider.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- Vertical -->
+  <div
+    v-if="vertical"
+    class="inline-block w-px self-stretch bg-gray-200"
+    :class="props.customizeClass"
+    role="separator"
+    aria-orientation="vertical"
+  />
+  <!-- Horizontal -->
+  <div
+    v-else
+    class="w-full h-px bg-gray-200"
+    :class="props.customizeClass"
+    role="separator"
+    aria-orientation="horizontal"
+  />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    vertical?: boolean;
+    customizeClass?: string;
+  }>(),
+  {
+    vertical: false,
+    customizeClass: "",
+  }
+);
+</script>
